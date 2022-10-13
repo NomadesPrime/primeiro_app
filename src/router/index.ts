2b1f5d76fbc222f047b32de2ab171e5d67f639dd
NomadesPrime <fernandesrichard312@gmail.com>
2022-10-13 15:56:32 -0300

    primeiro commit

---
+++ src/router/index.ts
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import TabsPage from '../views/TabsPage.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/tabs/tab1'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tab1'
+      },
+      {
+        path: 'tab1',
+        component: () => import('@/views/Foto.vue')
+      },
+      {
+        path: 'tab2',
+        component: () => import('@/views/JogosPage.vue')
+      },
+      {
+        path: 'tab3',
+        component: () => import('@/views/HomePage.vue')
+      },
+      {
+        path: 'tab4',
+        component: () => import('@/views/CafePage.vue')
+      }
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/TabsPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="camera" />
+          <ion-label>Fotos</ion-label>
+        </ion-tab-button>
+          
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="gameControllerOutline" />
+          <ion-label>Jogos</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="homeOutline" />
+          <ion-label>Home</ion-label>
+        </ion-tab-button>
+
+             
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="cafeOutline" />
+          <ion-label>Cafe</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { gameControllerOutline, camera, homeOutline, cafeOutline} from 'ionicons/icons';
+
+
+export default defineComponent({
+  name: 'TabsPage',
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  setup() {          
+    return {
+      gameControllerOutline, 
+      camera, 
+      homeOutline,
+      cafeOutline,
+    }
+  }
+});
+</script>
